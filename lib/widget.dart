bbe440ff2eb6eaf9932608506d6b7d11caa2b7fd
anasxhamsi <98109997+anasxhamsi@users.noreply.github.com>
2025-07-19 17:12:56 +0500

    learn row and column

--- lib/widget.dart
+++ lib/widget.dart
@@ -21,12 +21,85 @@ class WidgetHelper extends StatelessWidget {
           IconButton(onPressed: () {}, icon: Icon(Icons.add)),
         ],
       ),
-      body: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          color: Colors.blue,
-          child: Text("Anas", style: TextStyle(color: Colors.white)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    height: 200,
+                    width: 200,
+                    color: Colors.amber,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    height: 200,
+                    width: 200,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    height: 200,
+                    width: 200,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    height: 200,
+                    width: 200,
+                    color: Colors.grey,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    height: 200,
+                    width: 200,
+                    color: Colors.black54,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    height: 200,
+                    width: 200,
+                    color: Colors.cyanAccent,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    height: 200,
+                    width: 200,
+                    color: Colors.amber,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    height: 200,
+                    width: 200,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 11),
+              height: 200,
+              color: Colors.red,
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 11),
+              height: 200,
+              color: Colors.blue,
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 11),
+              height: 200,
+              color: Colors.grey,
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 11),
+              height: 200,
+              color: Colors.blue,
+            ),
+          ],
         ),
       ),
     );
